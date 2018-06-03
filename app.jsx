49315fba6f19da8636893bb37badf2a06d3e73d5
Bryant Vail <Bryanttv@outlook.com>
2018-06-03 16:36:51 -0700

    eslint,

---
+++ app.jsx
@@ -0,0 +1,28 @@
+const contentNode = document.getElementById('contents');
+
+class IssueList extends React.Component{
+    render(){
+        return(
+            <div>This is a Placeholder for the issue list.</div>
+        );
+    }//end render
+}
+
+ReactDom.render(<IssueList />, contentNode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
