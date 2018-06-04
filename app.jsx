fb1246cafe5a3907d472d5797ccfdce61ffecf69
Bryant Vail <Bryanttv@outlook.com>
2018-06-03 23:09:54 -0400

    save

--- app.jsx
+++ app.jsx
@@ -4,6 +4,7 @@ class IssueList extends React.Component{
     render(){
         return(
             <div>This is a Placeholder for the issue list.</div>
+            <div>Next Line</div>
         );
     }//end render
 }
